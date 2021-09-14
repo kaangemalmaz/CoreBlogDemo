604d6dfead1dea09d9b69bf7c363193aeed60b89
DESKTOP-46BN8NO\Kaan <n0syste3m65@hotmail.com>
2021-09-14 19:25:47 +0300

    14.09.2021 - MY

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T:class
+    {
+        List<T> ListAllCategory();
+        T GetByIdCategory(int id);
+        void AddCategory(T entity);
+        void UpdateCategory(T entity);
+        void DeleteCategory(T entity);
+    }
+}
